18d50ccd52f6e3f238c06b528d6ed973725e58d5
Rainzler Exagone <Yasser458891@gmail.com>
2023-06-04 17:47:02 +0200

    changes

--- RemoveDuplicate.py
+++ RemoveDuplicate.py
@@ -1,11 +1,8 @@
-# class Solution:
-#     def removeDuplicates(self, nums: list[int]) -> int:
-nums: list[int]    
-list[6] = {1,3,2,1,4,9}
-for i in range(len(nums)):
-  for j in range(len(nums)-1):
-   if(nums[i]==nums[j]):
-     nums[j]= nums[j]+1 
-   j+=1
-for i in range(len(nums)):
- print(nums[i])
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+       for i in range(len(nums)):
+         for j in range(i+1,len(nums)-1):
+            if (nums[i] == nums[j]):
+                 del nums[j]
+            
+         return len(nums) , nums
